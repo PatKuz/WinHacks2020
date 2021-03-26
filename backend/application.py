cdcb242fb96798cf258f190102daa65a7926a27a
PatKuz <>
2021-03-26 17:54:18 -0400

    tentative changes

--- backend/application.py
+++ backend/application.py
@@ -1,6 +1,16 @@
 from flask import Flask
 app = Flask(__name__)
 
+class question:
+    def __init__(self):
+        self.UPVOTES = 0
+
+    def addVote(self):
+        self.UPVOTES += 1
+
+    def undoVote(self):
+        self.UPVOTES -= 1
+
 @app.route('/')
 def hello_world():
     return 'Hello, World!'

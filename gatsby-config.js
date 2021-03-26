d0f557ec9eb22613d4ee43ad12e0e530ea425966
Victor Vicente <vitor@bu.edu>
2021-03-26 19:38:55 -0400

    Final Updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,37 @@
 module.exports = {
   siteMetadata: {
-    title: "ClassConnect",
+    title: `ClassConnect`,
+    description: `A Zoom assistance tool.`,
+    author: `Bro Code`,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Libre Baskerville`
+        ],
+        display: 'swap'
+      }
+    },
+    {
+      resolve: `gatsby-styled-components-dark-mode`,
+      options: {
+        light: require(`${__dirname}/src/styles/theme.js`).lightTheme,
+        dark: require(`${__dirname}/src/styles/theme.js`).darkTheme,
+      },
+    },
+  ],
 };

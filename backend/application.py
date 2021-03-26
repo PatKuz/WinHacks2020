fd30a5f87d3aecead4f24f74a2785bbeb19f874d
Conor Walsh <cewalsh@bu.edu>
2021-03-26 17:04:38 -0400

    Added route

--- backend/application.py
+++ backend/application.py
@@ -1,6 +1,15 @@
 from flask import Flask
+from keygen import generate
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/getCode')
+def get_code():
+    return generate()
+
+
+

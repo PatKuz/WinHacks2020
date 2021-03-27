0773eac73edb364c83533e74bfa4ae1772d27aed
Conor Walsh <cewalsh@bu.edu>
2021-03-26 20:29:34 -0400

    Upvote and get questions from db

--- backend/application.py
+++ backend/application.py
@@ -5,6 +5,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from cockroachdb.sqlalchemy import run_transaction
 import json
+from flask import jsonify
+from uuid import UUID
 
 
 with open('backend/creds.json') as (f):
@@ -25,7 +27,7 @@ seen_account_ids = set()
 app = Flask(__name__)
 
 class Question(Base):
-    """The Account class corresponds to the "accounts" database table.
+    """The Question class corresponds to the "qdata" database table.
     """
     __tablename__ = 'qdata'
     id = Column(Integer, primary_key=True)
@@ -59,11 +61,50 @@ def test():
     return Response("Yes", status=200)
 
 
-def testtable(session):
+def getQDB(session):
     sources = session.query(Question)
-    for source in sources:
-        print("Here")
-        print(source.id)
-        print(source.votes)
-        print(source.question)
+    # for source in sources:
+    #     print("Here")
+    #     print(source.id)
+    #     print(source.votes)
+    #     print(source.question)
     return sources
+
+
+
+@app.route('/testinsert')
+def testinsert():
+    run_transaction(sessionmaker(bind=engine),
+                lambda s: createQuestion(s, "Test5"))
+    return Response("Yes", status=200)
+    
+def createQuestion(session, content):
+    question = Question(
+        question = content,
+        votes = 1
+    )
+    session.add(question)
+
+@app.route('/getQuestions')
+def getQuestions():
+    results = run_transaction(sessionmaker(bind=engine), getQDB)
+    ret = []
+    for result in results:
+        ret.append({'id' : str(result.id), 'question' : result.question, 'votes' : result.votes})
+    
+    ret.sort(key=lambda s: s['votes'], reverse=True)
+
+    return Response({'questions' : ret}, status=200)
+
+
+
+def upvote(session, questionid):
+    session.query(Question).filter_by(id=questionid).update(
+        {"votes": (Question.votes + 1)}
+    )
+
+@app.route('/updateVotes/<questionid>')
+def updateVote(questionid):
+    print("This is called")
+    results = results = run_transaction(sessionmaker(bind=engine), lambda s: upvote(s, questionid))
+    return Response("Yes", status=200)

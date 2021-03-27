9aab429d15678e27143553ff5d56f8d84a6fe938
PatKuz <>
2021-03-26 20:57:07 -0400

    updated backend

--- backend/application.py
+++ backend/application.py
@@ -15,7 +15,7 @@ with open('backend/creds.json') as (f):
 password = str(creds['PASS'])
 Base = declarative_base()
 engine = create_engine(
-    'cockroachdb://winhacks21:' + password +  '@class-connect-8nw.gcp-us-east4.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&sslrootcert=backend\certs\class-connect-ca.crt',
+    'cockroachdb://winhacks21:' + password +  '@class-connect-8nw.gcp-us-east4.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full&sslrootcert=backend/certs/class-connect-ca.crt',
     echo=True                   # Log SQL queries to stdout
 )
 
@@ -34,20 +34,9 @@ class Question(Base):
     question = Column(String)
     votes = Column(Integer)
 
-class questio:
-    def __init__(self):
-        self.UPVOTES = 0
-
-    def addVote(self):
-        self.UPVOTES += 1
-
-    def undoVote(self):
-        self.UPVOTES -= 1
-
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
-
 
 @app.route('/getCode')
 def gen_key():
@@ -63,21 +52,35 @@ def test():
 
 def getQDB(session):
     sources = session.query(Question)
-    # for source in sources:
-    #     print("Here")
-    #     print(source.id)
-    #     print(source.votes)
-    #     print(source.question)
     return sources
 
 
+@app.route('/droptable/<key>')
+def droptable(key):
+    results = run_transaction(sessionmaker(bind=engine), lambda s: drop(s, key))
+    return Response("Yes", status=200)
+
+def drop(session, key):
+    session.execute('DROP TABLE ' + str(key))
+
+@app.route('/createtable/<key>')
+def createtable(key):
+    results = run_transaction(sessionmaker(bind=engine), lambda s: create(s, key))
+    return Response("Yes", status=200)
+
+def create(session, key):
+    session.execute('CREATE TABLE ' + str(key) + ''' (
+            ID UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+            QUESTION STRING,
+            VOTES INT
+            );''')
 
 @app.route('/testinsert')
 def testinsert():
     run_transaction(sessionmaker(bind=engine),
                 lambda s: createQuestion(s, "Test5"))
     return Response("Yes", status=200)
-    
+
 def createQuestion(session, content):
     question = Question(
         question = content,
@@ -91,7 +94,7 @@ def getQuestions():
     ret = []
     for result in results:
         ret.append({'id' : str(result.id), 'question' : result.question, 'votes' : result.votes})
-    
+
     ret.sort(key=lambda s: s['votes'], reverse=True)
 
     return Response({'questions' : ret}, status=200)
@@ -103,8 +106,13 @@ def upvote(session, questionid):
         {"votes": (Question.votes + 1)}
     )
 
+def removeVote(session, questionid):
+    session.query(Question).filter_by(id=questionid).update(
+        {"votes": (Question.votes - 1)}
+    )
+
 @app.route('/updateVotes/<questionid>')
 def updateVote(questionid):
     print("This is called")
-    results = results = run_transaction(sessionmaker(bind=engine), lambda s: upvote(s, questionid))
+    results = run_transaction(sessionmaker(bind=engine), lambda s: upvote(s, questionid))
     return Response("Yes", status=200)

4b4ebfa49535afba77395c32511face171dfd90d
Victor Vicente <vitor@bu.edu>
2021-03-27 18:03:05 -0400

    ESLint /pages

--- src/pages/control-panel.js
+++ src/pages/control-panel.js
@@ -1,110 +1,112 @@
-import * as React from "react"
+import * as React from "react";
 import { navigate } from "gatsby";
-import styled from 'styled-components'
+import styled from "styled-components";
 import { compose } from "recompose";
 
-import {withFirebase, withAuthorization} from "../api/"
+import { withFirebase, withAuthorization } from "../api/";
 import { Centered } from "../styles/global";
 import TeacherRoom from "../components/TeacherRoom";
 import Logo from "../components/Logo";
 import SEO from "../components/SEO";
-import Room from "../components/Room";
 
 import Background from "../images/home-background.png";
 
 const StyledCentered = styled(Centered)`
-	background: url(${Background});
+  background: url(${Background});
 `;
 
 const isProfessor = (authUser) => !!authUser?.roles?.professor;
 
 class ControlPage extends React.Component {
-	_initFirebase = false;
-	state = {
-		roomCode: "",
-		rooms: null,
-	}
-	unsubRooms = null;
-	
-    componentDidMount() {
-		if (this.props.firebase && !this._initFirebase) this.loadData();
-	}
-	
-    componentDidUpdate(prevProps) {
-		if (this.props.firebase && !this._initFirebase) this.loadData();
-	}
-	
-    componentWillUnmount() {
-		if (typeof this.unsubRooms === "function") this.unsubRooms();
-	}
-	
-	loadData = async () => {
-		this._initFirebase = true;
-		
-		const rooms = await new Promise((resolve, reject) => {
-			let resolveOnce = (doc) => {
-				resolveOnce = () => null;
-				resolve(doc);
-			};
-			
-			this.unsubRooms = this.props.firebase
-				.rooms()
-				.onSnapshot((querySnapshot) => {
-					const rooms = querySnapshot.docs.map((doc) => {
-						return {
-							id: doc.id,
-							...doc.data(),
-						};
-					});
-					this.setState({ rooms });
-					resolveOnce(rooms);
-				}, reject);
-		});
-	}
-	
-	setRoomCode = (roomCode) => {
-		this.setState({roomCode})
-	};
-	
-	attemptLogin = (login) => {
-		this.props.firebase.doSignInWithEmail(login.email, login.password).then(() => {
-			navigate("/control-panel");
-		}).catch((err) => console.error(err));
-	};
-	
-	render() {
-		const { rooms, roomCode } = this.state;
-		
-		
-		const room = rooms ? rooms.find((r) => r.id === roomCode) : null;
-		
-		if (roomCode === "")
-			return (
-			    <>
-					<SEO title="Home" route="/" />
-					<StyledCentered>
-						<Logo size="large"/>
-					</StyledCentered>
-				</>
-			);
-		else if (room === undefined || room === null)
-				return (
-			    <>
-					<SEO title="Home" route="/" />
-					<StyledCentered>
-						<Logo size="large"/>
-						<h1> Room not found! </h1>
-					</StyledCentered>
-				</>
-				);
-		else
-			return (
-			    <>
-					<SEO title="Room" route="/" />
-					<TeacherRoom room={room}/>
-				</>
-			);
-	}
+  _initFirebase = false;
+  state = {
+    roomCode: "",
+    rooms: null,
+  };
+  unsubRooms = null;
+
+  componentDidMount() {
+    if (this.props.firebase && !this._initFirebase) this.loadData();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.firebase && !this._initFirebase) this.loadData();
+  }
+
+  componentWillUnmount() {
+    if (typeof this.unsubRooms === "function") this.unsubRooms();
+  }
+
+  loadData = async () => {
+    this._initFirebase = true;
+
+    // eslint-disable-next-line
+    const rooms = await new Promise((resolve, reject) => {
+      let resolveOnce = (doc) => {
+        resolveOnce = () => null;
+        resolve(doc);
+      };
+
+      this.unsubRooms = this.props.firebase
+        .rooms()
+        .onSnapshot((querySnapshot) => {
+          const rooms = querySnapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          this.setState({ rooms });
+          resolveOnce(rooms);
+        }, reject);
+    });
+  };
+
+  setRoomCode = (roomCode) => {
+    this.setState({ roomCode });
+  };
+
+  attemptLogin = (login) => {
+    this.props.firebase
+      .doSignInWithEmail(login.email, login.password)
+      .then(() => {
+        navigate("/control-panel");
+      })
+      .catch((err) => console.error(err));
+  };
+
+  render() {
+    const { rooms, roomCode } = this.state;
+
+    const room = rooms ? rooms.find((r) => r.id === roomCode) : null;
+
+    if (roomCode === "")
+      return (
+        <>
+          <SEO title="Home" route="/" />
+          <StyledCentered>
+            <Logo size="large" />
+          </StyledCentered>
+        </>
+      );
+    else if (room === undefined || room === null)
+      return (
+        <>
+          <SEO title="Home" route="/" />
+          <StyledCentered>
+            <Logo size="large" />
+            <h1> Room not found! </h1>
+          </StyledCentered>
+        </>
+      );
+    else
+      return (
+        <>
+          <SEO title="Room" route="/" />
+          <TeacherRoom room={room} />
+        </>
+      );
+  }
 }
 
 export default compose(
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import styled from 'styled-components'
+import styled from "styled-components";
 import { navigate } from "gatsby";
 
-import {withFirebase} from "../api/"
+import { withFirebase } from "../api/";
 import { Centered } from "../styles/global";
 import Card from "../components/Card";
 import Logo from "../components/Logo";
@@ -12,166 +12,197 @@ import Room from "../components/Room";
 import Background from "../images/pattern_3.svg";
 
 const StyledCentered = styled(Centered)`
-	background: url(${Background});
+  background: url(${Background});
 `;
 
 class IndexPage extends Component {
-	constructor(props) {
-		super(props);
+  constructor(props) {
+    super(props);
 
-		this.state = {
-			roomCode: "",
-			rooms: null,
-			errorMsg: "",
-		}
+    this.state = {
+      roomCode: "",
+      rooms: null,
+      errorMsg: "",
+    };
 
-		this._initFirebase = false;
-		this.unsubRooms = null;
-	}
+    this._initFirebase = false;
+    this.unsubRooms = null;
+  }
 
-    componentDidMount() {
-		if (this.props.firebase && !this._initFirebase) this.loadData();
-	}
+  componentDidMount() {
+    if (this.props.firebase && !this._initFirebase) this.loadData();
+  }
 
-    componentDidUpdate(prevProps) {
-		if (this.props.firebase && !this._initFirebase) this.loadData();
-	}
+  componentDidUpdate(prevProps) {
+    if (this.props.firebase && !this._initFirebase) this.loadData();
+  }
 
-    componentWillUnmount() {
-		if (typeof this.unsubRooms === "function") this.unsubRooms();
-	}
+  componentWillUnmount() {
+    if (typeof this.unsubRooms === "function") this.unsubRooms();
+  }
 
-	loadData = async () => {
-		this._initFirebase = true;
-		
-		let storedRoomCode = localStorage.getItem("roomCode");
-		
-		if (storedRoomCode !== null && storedRoomCode !== undefined)
-			this.setState({ roomCode: storedRoomCode })
+  loadData = async () => {
+    this._initFirebase = true;
 
-		const rooms = await new Promise((resolve, reject) => {
-			let resolveOnce = (doc) => {
-				resolveOnce = () => null;
-				resolve(doc);
-			};
+    let storedRoomCode = localStorage.getItem("roomCode");
 
-			this.unsubRooms = this.props.firebase
-				.rooms()
-				.onSnapshot((querySnapshot) => {
-					const rooms = querySnapshot.docs.map((doc) => {
-						return {
-							id: doc.id,
-							...doc.data(),
-						};
-					});
-					this.setState({ rooms });
-					resolveOnce(rooms);
-				}, reject);
-		});
-	}
+    if (storedRoomCode !== null && storedRoomCode !== undefined)
+      this.setState({ roomCode: storedRoomCode });
 
-	setRoomCode = (roomCode) => {
-		this.setErrorMsg("");
-		
-		localStorage.setItem("roomCode", roomCode);
-		
-		this.setState({roomCode: roomCode})
-	};
+    // eslint-disable-next-line
+    const rooms = await new Promise((resolve, reject) => {
+      let resolveOnce = (doc) => {
+        resolveOnce = () => null;
+        resolve(doc);
+      };
 
-	setErrorMsg = (errorMsg) => {
-		this.setState({errorMsg: errorMsg});
-		this.forceUpdate();
-	}
+      this.unsubRooms = this.props.firebase
+        .rooms()
+        .onSnapshot((querySnapshot) => {
+          const rooms = querySnapshot.docs.map((doc) => {
+            return {
+              id: doc.id,
+              ...doc.data(),
+            };
+          });
+          this.setState({ rooms });
+          resolveOnce(rooms);
+        }, reject);
+    });
+  };
 
-	attemptLogin = (email, password) => {
-		this.setErrorMsg("");
-		this.props.firebase.doSignInWithEmail(email, password).then(() => {
-			navigate("/control-panel");
-		}).catch((err) => {
-			console.log(err);
-			this.setErrorMsg("Login Failed!");
-		});
-	};
+  setRoomCode = (roomCode) => {
+    this.setErrorMsg("");
 
-	attemptRegister = (name, email, password) => {
-		this.setErrorMsg("");
-		this.props.firebase.doRegisterWithEmail(email, password).then((userCredential) => {
-			const user = userCredential.user;
-			
-			this.props.firebase.user(user.uid).set({name: name, email: email, roles: { professor: true }}).then(() => {
-				this.attemptLogin(email, password);
-			}).catch((err) => {
-				console.log(err);
-				this.setErrorMsg("User Creation Failed!");
-			});
-		});
-	};
-	
-	addQuestion = async (roomID, questionID, question, studentID) => {
-		const {rooms} = this.state;
-		
-		const room = rooms.find((r) => r.id === roomID);
-		
-		room.questions[questionID] = question;
-		
-		this.props.firebase.room(roomID).set(room).catch((err) => {
-			console.log(err)
-		});
-	};
-	
-	upvoteQuestion = (roomID, questionID, studentID) => {
-		
-	};
-	
-	exitRoom = () => {
-		this.setRoomCode("");
-	};
+    localStorage.setItem("roomCode", roomCode);
 
-	render() {
-		let { rooms, roomCode, errorMsg } = this.state;
+    this.setState({ roomCode: roomCode });
+  };
 
-		const room = rooms ? rooms.find((r) => r.id === roomCode) : null;
+  setErrorMsg = (errorMsg) => {
+    this.setState({ errorMsg: errorMsg });
+    this.forceUpdate();
+  };
 
-		if (errorMsg !== "")
-			return (
-			    <>
-					<SEO title="Home" route="/" />
-					<StyledCentered>
-						<Logo size="large"/>
-						<h1> {errorMsg} </h1>
-						<Card setRoomCode={this.setRoomCode} attemptLogin={this.attemptLogin} attemptRegister={this.attemptRegister} addQuestion={this.addQuestion} />
-					</StyledCentered>
-				</>
-			);
-		else if (roomCode === "")
-			return (
-			    <>
-					<SEO title="Home" route="/" />
-					<StyledCentered>
-						<Logo size="large"/>
-						<Card setRoomCode={this.setRoomCode} attemptLogin={this.attemptLogin} attemptRegister={this.attemptRegister} addQuestion={this.addQuestion} />
-					</StyledCentered>
-				</>
-			);
-		else if (room === null || room === undefined)
-			return (
-			    <>
-					<SEO title="Home" route="/" />
-					<StyledCentered>
-						<Logo size="large"/>
-						<h1> Room not found! </h1>
-						<Card setRoomCode={this.setRoomCode} attemptLogin={this.attemptLogin} attemptRegister={this.attemptRegister} addQuestion={this.addQuestion} />
-					</StyledCentered>
-				</>
-			);
-		else
-			return (
-			    <>
-					<SEO title="Room" route="/" />
-					<Room room={room} addQuestion={this.addQuestion} upvoteQuestion={this.upvoteQuestion} exitRoom={this.exitRoom} />
-				</>
-			);
-	}
+  attemptLogin = (email, password) => {
+    this.setErrorMsg("");
+    this.props.firebase
+      .doSignInWithEmail(email, password)
+      .then(() => {
+        navigate("/control-panel");
+      })
+      .catch((err) => {
+        console.log(err);
+        this.setErrorMsg("Login Failed!");
+      });
+  };
+
+  attemptRegister = (name, email, password) => {
+    this.setErrorMsg("");
+    this.props.firebase
+      .doRegisterWithEmail(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+
+        this.props.firebase
+          .user(user.uid)
+          .set({ name: name, email: email, roles: { professor: true } })
+          .then(() => {
+            this.attemptLogin(email, password);
+          })
+          .catch((err) => {
+            console.log(err);
+            this.setErrorMsg("User Creation Failed!");
+          });
+      });
+  };
+
+  addQuestion = async (roomID, questionID, question, studentID) => {
+    const { rooms } = this.state;
+
+    const room = rooms.find((r) => r.id === roomID);
+
+    room.questions[questionID] = question;
+
+    this.props.firebase
+      .room(roomID)
+      .set(room)
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  upvoteQuestion = (roomID, questionID, studentID) => {};
+
+  exitRoom = () => {
+    this.setRoomCode("");
+  };
+
+  render() {
+    let { rooms, roomCode, errorMsg } = this.state;
+
+    const room = rooms ? rooms.find((r) => r.id === roomCode) : null;
+
+    if (errorMsg !== "")
+      return (
+        <>
+          <SEO title="Home" route="/" />
+          <StyledCentered>
+            <Logo size="large" />
+            <h1> {errorMsg} </h1>
+            <Card
+              setRoomCode={this.setRoomCode}
+              attemptLogin={this.attemptLogin}
+              attemptRegister={this.attemptRegister}
+              addQuestion={this.addQuestion}
+            />
+          </StyledCentered>
+        </>
+      );
+    else if (roomCode === "")
+      return (
+        <>
+          <SEO title="Home" route="/" />
+          <StyledCentered>
+            <Logo size="large" />
+            <Card
+              setRoomCode={this.setRoomCode}
+              attemptLogin={this.attemptLogin}
+              attemptRegister={this.attemptRegister}
+              addQuestion={this.addQuestion}
+            />
+          </StyledCentered>
+        </>
+      );
+    else if (room === null || room === undefined)
+      return (
+        <>
+          <SEO title="Home" route="/" />
+          <StyledCentered>
+            <Logo size="large" />
+            <h1> Room not found! </h1>
+            <Card
+              setRoomCode={this.setRoomCode}
+              attemptLogin={this.attemptLogin}
+              attemptRegister={this.attemptRegister}
+              addQuestion={this.addQuestion}
+            />
+          </StyledCentered>
+        </>
+      );
+    else
+      return (
+        <>
+          <SEO title="Room" route="/" />
+          <Room
+            room={room}
+            addQuestion={this.addQuestion}
+            upvoteQuestion={this.upvoteQuestion}
+            exitRoom={this.exitRoom}
+          />
+        </>
+      );
+  }
 }
 
 export default withFirebase(IndexPage);

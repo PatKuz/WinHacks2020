1e09ffa247d91d70cca9bf665eca9090ba235d13
PatKuz <>
2021-03-27 16:00:43 -0400

    changes

--- src/components/Register.js
+++ src/components/Register.js
@@ -28,11 +28,24 @@ const StyledFormControl = styled(Form.Control)`
   display: inline-block;
 `;
 
+const StyledFormControlz = styled(Form.Control)`
+  background-color: #ffffff;
+  position: relative;
+  text-decoration: none;
+  border: 0.5px solid light-blue;
+  width: 400px;
+  height: 75px;
+  margin-left: 22px;
+  font-family: 'Overpass', sans-serif;
+  font-size: 20px;
+  display: inline-block;
+`;
+
 const StyledButton = styled(Button)`
-  margin-left: 90%;
+  margin-left: 89%;
   border: none;
   color: #242424;
-  padding: 15px 32px;
+  padding: 15px 25px;
   text-align: center;
   text-decoration: none;
   display: inline-block;
@@ -42,9 +55,11 @@ const StyledButton = styled(Button)`
   cursor: pointer;
   font-size: 15px;
   border-radius: 3px;
+  border: 2px solid #f2f3f4;
   &:hover {
-	background-color: #ffffff;
-	color: #242424;
+    background-color: #f2f3f4;
+    color: #242424;
+    border: 2px solid #242424;
   };
 `;
 
@@ -54,27 +69,17 @@ class Register extends React.Component {
 		password: "",
 		name: "",
 	}
-	
-	
+
+
 	render() {
 	  const {email, password, name} = this.state;
-		
+
 	  return (
 		<>
             <Form>
-              <StyledFormGroup controlId="nameField">
-			    Name
-                <StyledFormControl
-                  type="text"
-                  value={name}
-                  onChange={(e) =>
-                    this.setState({ name: e.target.value })
-                  }
-                />
-              </StyledFormGroup>
               <StyledFormGroup controlId="emailField">
-			    Email
-                <StyledFormControl
+			     Email
+                <StyledFormControlz
                   type="email"
                   value={email}
                   onChange={(e) =>
@@ -92,8 +97,18 @@ class Register extends React.Component {
                   }
                 />
               </StyledFormGroup>
+              <StyledFormGroup controlId="nameField">
+          Name
+                <StyledFormControl
+                  type="text"
+                  value={name}
+                  onChange={(e) =>
+                    this.setState({ name: e.target.value })
+                  }
+                />
+              </StyledFormGroup>
             </Form>
-			
+
             <StyledButton
               variant="primary"
               onClick={() =>

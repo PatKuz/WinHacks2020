cfbcccca17571f8bf4def34fe006d52c065342b3
Victor Vicente <vitor@bu.edu>
2021-03-27 18:11:08 -0400

    Fixes 2.0

--- src/components/TeacherRoom.js
+++ src/components/TeacherRoom.js
@@ -1,7 +1,7 @@
 import * as React from "react";
 import styled from "styled-components";
 
-import Background from "../images/home-background.png";
+import Background from "../images/pattern_3.svg";
 import { Button, Modal } from "react-bootstrap";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";

b439e269b885150c9aac73c5d5a69d182c9291f4
Conor Walsh <cewalsh@bu.edu>
2021-03-28 05:26:47 -0400

    Cleaned up

--- backend/application.py
+++ backend/application.py
@@ -55,27 +55,6 @@ class Question(Base):
     votes = Column(Integer)
 
 
-@app.route('/', methods=['GET'])
-def hello_world():
-    return Response({'code' : 3}, status=200)
-
-@app.route('/getCode', methods=['GET'])
-def gen_key():
-    import secrets
-    return Response({'code' : secrets.token_hex(3)}, status=200)
-
-
-@app.route('/testtable')
-def test():
-    print(run_transaction(sessionmaker(bind=engine), testtable))
-    return Response("Yes", status=200)
-
-
-def getQDB(session):
-    sources = session.query(Question)
-    return sources
-
-
 @app.route('/droptable/<key>')
 def droptable(key):
     results = run_transaction(sessionmaker(bind=engine), lambda s: drop(s, key))
@@ -97,54 +76,11 @@ def create(session, key):
             VOTES INT
             );''')
 
-@app.route('/testinsert')
-def testinsert():
-    run_transaction(sessionmaker(bind=engine),
-                lambda s: createQuestion(s, "Test5"))
-    return Response("Yes", status=200)
-
-def createQuestion(session, content):
-    question = Question(
-        question = content,
-        votes = 1
-    )
-    session.add(question)
-
-@app.route('/getQuestions')
-def getQuestions():
-    results = run_transaction(sessionmaker(bind=engine), getQDB)
-    ret = []
-    for result in results:
-        ret.append({'id' : str(result.id), 'question' : result.question, 'votes' : result.votes})
-
-    ret.sort(key=lambda s: s['votes'], reverse=True)
-
-    return Response({'questions' : ret}, status=200)
-
-
-
-def upvote(session, questionid):
-    session.query(Question).filter_by(id=questionid).update(
-        {"votes": (Question.votes + 1)}
-    )
-
-def removeVote(session, questionid):
-    session.query(Question).filter_by(id=questionid).update(
-        {"votes": (Question.votes - 1)}
-    )
-
-@app.route('/updateVotes/<questionid>')
-def updateVote(questionid):
-    print("This is called")
-    results = run_transaction(sessionmaker(bind=engine), lambda s: upvote(s, questionid))
-    return Response("Yes", status=200)
-
 
 
 @app.route('/downloadSession/<uuid>')
 def downloadStuff(uuid):
-    print("This is being downloaded") 
-    print(uuid)
+
     uuid = 'a' + uuid.replace('-','').replace('\"','')
     
     data = {'table_name' : uuid}
@@ -152,24 +88,13 @@ def downloadStuff(uuid):
     with open('backend/table_name.json', 'w') as (f):
                 json.dump(data, f, ensure_ascii=False, indent=4)
 
-   
     table_name = uuid
-
-    
     results = run_transaction(sessionmaker(bind=engine), lambda s: download(s, uuid))
-    print(results)
+
     return Response({'Results' : (results)}, status=200)
 
 def download(session, uuid):
-    # session.execute('SELECT * FROM abd239ec84740426ba99fe82568cf799d')
-    # rows = session.fetchall()
-    # session.commit()
-    # for r in rows:
-    #     print(r)
-    print("Downloading")
-    print(getTableName())
     sources = session.query(Question).all()
-    print(sources)
     ret = []
     for result in sources:
         ret.append({'id' : str(result.id), 'title':result.title, 'question' : result.question, 'votes' : result.votes})
@@ -179,10 +104,7 @@ def download(session, uuid):
 
 @app.route('/insertSession', methods=['POST'])
 def insertSession():
-    print("Here")
-    print(request)
     form = request.form
-    print(form)
 
     roomid = form["id"]
     roomName = form["roomName"]
@@ -191,12 +113,9 @@ def insertSession():
 
     uuid = 'a' + uuid.replace('-','').replace('\"','')
     table_name = uuid
-    print("uuid")
-    print(uuid)
 
-    print(questions)
     questions = json.loads(questions)
-    print(questions)
+
 
     addStuffTODB(roomid, roomName, uuid, questions)
 
@@ -204,20 +123,13 @@ def insertSession():
 
 
 def addStuffTODB(roomId, roomName, uuid, questions):
-    print("Making table")
     run_transaction(sessionmaker(bind=engine), lambda s: create(s, uuid))
-    print("printing questions")
     for question in questions:
         run_transaction(sessionmaker(bind=engine), lambda s: insert(s, uuid, question['title'], question['description'], question['votes']))
-        print("HERE WE ARE")
-        # print(question)
-
 
 
 
 def insert(session, uuid, title, question, votes):
-    print(uuid)
-
     x = random.randint(1,1000)
 
     sql = 'INSERT INTO ' + uuid + ' (ID,TITLE, QUESTION, VOTES) VALUES (:iu, :name, :ques, :vote)'
@@ -227,10 +139,3 @@ def insert(session, uuid, title, question, votes):
         "ques":question,
         "vote":votes
     })
-    # question = Question(
-    #     id=3,
-    #     title = title,
-    #     question = question,
-    #     votes = votes
-    # )
-    # session.add(question)
